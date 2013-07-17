184ed1198aa3f3c0512f04260164278ba28b925e
divya <divya@vinsol.com>
2013-07-17 16:04:03 +0530

    review

--- exercise5/js/tabs.js
+++ exercise5/js/tabs.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
   $list.insertBefore($module.eq(0));
   //3.Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to   the unordered list element.
   $module.each(function( index ) {
-    var currentDiv=$(this);
-    $content = $module.eq(index).find('h2').text();
+    var $currentDiv=$(this);
+    $content = $currentDiv.find('h2').text();
     $listItem = $("<li> " + $content + "</li>").appendTo($list);
     //4.Bind a click event to the list item that:Shows the related module, and hides any other modules , Adds a class of "current" to the clicked list item , Removes the class "current" from the other list item
     $listItem.click(function( e) {
       $(this).addClass('current').siblings().removeClass('current');
-      currentDiv.show();
-      $module.not(currentDiv).hide();
+      $currentDiv.show();
+      $module.not($currentDiv).hide();
     });
  });
   $module.eq(0).show();
-  $list.children('li:first').addClass('current');
+  $list.children('li:first').addClass('current');                          //since the selected tab has a class current
 });
 
