f0ab3543ee26c3da1c60b1177c0afa7fedbce870
divya <divya@vinsol.com>
2013-07-22 11:18:23 +0530

    issues resolved

--- exercise3/js/sandbox.js
+++ exercise3/js/sandbox.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
   //solution to 1
+  var listArray = [];
+  var divArray = [];
   var list = $('#myList');
   for(var i = 0 ; i < 5 ; i++) {
-    list.append('<li>list item' + (i+1) +'</li>');
+    listArray.push('<li>item' + (i+1) +'</li>');
   }
+  $('#myList').append(listArray.join(''));
   //solution to 2
-  $('#myList li').filter(':odd').remove();
+  $('#myList li:odd').remove();
   //solution to 3
-  $('div.module:last').prepend('<h2> heading added </h2><p> paragraph added </p>');
+  divArray.push('<h2> heading added </h2><p> paragraph added </p>');
+  $('div.module:last').append(divArray.join(''));
   //solution to 4
   $('#specials select[name="day"]').append("<option value='wednesday'>wednesday</option>");
   //solution to 5
   $('<div class="module"/>').insertAfter('div.module:last');
-  $('img[src="images/vegetable.jpg"]').clone().appendTo('div.module:last');
+  $('*').find('img').first().clone().appendTo('div.module:last');
 });
