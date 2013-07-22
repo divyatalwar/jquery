5002ddf0a7c11e70501321e48535edbea6e2a4a3
divya <divya@vinsol.com>
2013-07-22 11:17:23 +0530

    issues resolved

--- exercise2/js/sandbox.js
+++ exercise2/js/sandbox.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
   alert("image" + (index + 1) + " alt attribute : " + $(this).attr('alt'));
   });
   //2
-  $('.input_text').parent().addClass('sampleClass');
+  $('.input[type="text"]').closest('form').addClass('sampleClass');
   //3
   $('#myList li.current').removeClass('current').next().addClass('current');
   //4
-  $('#specials select[name="day"]').parent().next().children();
+  alert($('#specials ').find('select[name="day"]').end().find('input[type="submit"]').val());   
   //5
   $('#slideshow li:first-child').addClass('current').siblings().addClass('disabled');
 });
