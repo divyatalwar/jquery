60b64ba67b1644be6b4231c090922559a6e7e95a
divya <divya@vinsol.com>
2013-07-23 13:29:57 +0530

    issues resolved

--- exercise11/exercise11.js
+++ exercise11/exercise11.js
@@ -2,12 +2,10 @@ $(document).ready(function () {
   var index =0;
   $('#add').click(function() {
     index = index +1;
-    var $newDiv=$('<div id=" ' + (index) + '"</div>');
-    var $target = ($newDiv).appendTo($('#container'))
-    $target.text("div id " + index);
-    $target.addClass('innerdiv');
+    var $newDiv=$('<div id=" ' + (index) + '" class="newDiv"</div>');
+    var $target = ($newDiv).appendTo($('#container')).text("div id " + index ).addClass('innerdiv');
   });
-  $('#container').delegate('div' , "click", function() {
+  $('#container').delegate('div.newDiv' , "click", function() {
     $(this).toggleClass('highlight');
     if($(this).is(':last-child')) {
     $(this).remove();
