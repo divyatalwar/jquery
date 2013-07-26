24edce4e0692d967de3d81c3302c3a181a1b9cee
divya <divya@vinsol.com>
2013-07-26 16:54:14 +0530

    library updated and issues resolved

--- exercise9/js/load.js
+++ exercise9/js/load.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     $(this).after($('<div>'));
     $targetDiv = $(this).next();
     $(this).data('ref' ,$targetDiv);
-    })
-    .click(function(e) {
-      e.preventDefault();
-      post = $(this).find('a').attr('href');
-      $(this).data('ref').load(post);
-    });  
-  });
+  })
+  .click(function(e) {
+    e.preventDefault();
+    post = $(this).find('a').attr('href').replace('#',' #');
+    $(this).data('ref').load(post);
+  });  
+});
