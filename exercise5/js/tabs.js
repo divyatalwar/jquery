e6627e42b62ccf71e87c95b990d23ca0f4e270eb
divya <divya@vinsol.com>
2013-07-26 14:40:49 +0530

    issues resolved

--- exercise5/js/tabs.js
+++ exercise5/js/tabs.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   //1 Hide all of the modules
   $module.hide();
   //2.Create an unordered list element before the first module.
-  $list = $("<ul> </ul>");
+  var $list = $("<ul> </ul>");
   $list.insertBefore($module.first());
   //3.Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to   the unordered list element.
   $module.each(function( index ) {
     var $currentDiv = $(this);
     var $content = $currentDiv.find('h2').text();
-    $listItem = $("<li> " + $content + "</li>").appendTo($list);
+    var $listItem = $("<li> " + $content + "</li>").appendTo($list);
     //4.Bind a click event to the list item that:Shows the related module, and hides any other modules , Adds a class of "current" to the clicked list item , Removes the class "current" from the other list item
     $listItem.click(function() {
       $(this).addClass('current').siblings().removeClass('current');
