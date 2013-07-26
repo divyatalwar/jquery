c1529354518761eba0b9bcd5d9cc8c765a2ce729
divya <divya@vinsol.com>
2013-07-26 16:14:15 +0530

    separate binding for product and rating

--- hiddenexercise3/hidden3.js
+++ hiddenexercise3/hidden3.js
@@ -1,23 +1,17 @@
 $(document).ready(function () {
   var array=[];
-  //if user simly clicks on the ratings wihtout selecting the product nothings happens.
+  //by default all radio buttons are disabled
+  $(":radio").attr("disabled", true);
+  // a click event bind to the rating parameters.
   $('td.ratingparameters').bind('click', function() {
-    //$(this).addClass('highlight').siblings().removeClass('highlight');
-    $(":radio").attr("disabled", true); 
-  });
-  //radio buttons are by default disabled.are only selected as per choices
-  $(":radio").attr("disabled", true); 
-  //a click function bind to the product and thereafter the rating
-  $('td.product').bind('click', function() {
-    $('td.ratingparameters').removeClass('highlight');
-    $(this).closest('#grid').find('.product').removeClass('highlight');
-    $(this).addClass('highlight');
-    $name = $(this).attr('id');
-    $('td.ratingparameters').bind('click', function() {
-      $(this).addClass('highlight').siblings().removeClass('highlight');
+    $(":radio").attr("disabled", true);
+    $(this).addClass('highlight').siblings().removeClass('highlight');
+    //if user simly clicks on the ratings wihtout selecting the product nothings happens.
+    if($('td.product.highlight').length > 0) {
+      $name=$('td.product.highlight').attr('id');
       $value = $(this).attr('id');
       $(":radio[value='" + $value + "'][name='" +$name+"']").attr("checked", true);
-      //checking if the radiobutton belongs to the same class..i.e. we are changing our ratings for a particular product and if it is we should make sure that only selected radiobuttons are enabled.
+      //checking if the radiobutton belongs to the same class..i.e. we are changing our ratings for a particular product and if it is we  should make sure that only selected radiobuttons are enabled.
       var index = array.map(function(el){ return el[1];}).indexOf($name);
       if(index >=0) {
         array.splice(index, 1);
@@ -27,6 +21,15 @@ $(document).ready(function () {
       for(i=0;i<array.length;i++){
         $(":radio[value='" + array[i][0] + "'][name='" +array[i][1] +"']").removeAttr('disabled');
       }
-    });
+    }
+    else {  
+      $(":radio").attr("disabled", true);
+    }
+  }); 
+  //a click function bind to the product
+  $('td.product').bind('click', function() {
+    $('td.ratingparameters').removeClass('highlight');
+    $(this).closest('#grid').find('.product').removeClass('highlight');
+    $(this).addClass('highlight');
   });
 });
