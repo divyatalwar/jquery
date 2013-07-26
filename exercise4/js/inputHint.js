eaaca933ab3559a045bbd54512edc00f0a09448e
divya <divya@vinsol.com>
2013-07-26 14:20:10 +0530

    code for blur and focus kept separate

--- exercise4/js/inputHint.js
+++ exercise4/js/inputHint.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   // 1. Set the value of the search input to the text of the label element
   var label = $("label[for='q']");
-  labelText = label.text();
+  var labelText = label.text();
   var searchInput = $('input[name="q"]');
   searchInput.val(labelText);
   //2.Add a class of "hint" to the search input
@@ -11,16 +11,13 @@ $(document).ready(function() {
   label.remove();
   //4.Bind a focus event to the search input that removes the hint text and the "hint" class
   //5.Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
-  searchInput.bind('focus blur' ,function(e) {
-    var $this = $(this);
-    $this.toggleClass('hint');
-    if(e.type == 'blur'&& $this.val() == '') {
-      $this.val(labelText);
-    }
-    else {
-      if($this.val() == labelText) {
-        $this.val("");
-      }
-    }
+  searchInput.bind('focus', function() {
+		$(this).val("").removeClass('hint');
+	})
+	.bind('blur', function() {	 
+	  if($(this).val()=="") {
+      $(this).val(labelText);
+    }      
+    $(this).addClass('hint');
   });
 });
