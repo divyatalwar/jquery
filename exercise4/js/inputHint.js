2d71b2c6fa18a1b98fa26e130631f67e32515b00
divya <divya@vinsol.com>
2013-07-27 13:06:25 +0530

    issues resolved

--- exercise4/js/inputHint.js
+++ exercise4/js/inputHint.js
@@ -4,20 +4,19 @@ $(document).ready(function() {
   var label = $("label[for='q']");
   var labelText = label.text();
   var searchInput = $('input[name="q"]');
-  searchInput.val(labelText);
   //2.Add a class of "hint" to the search input
-  searchInput.addClass('hint');
+  searchInput.val(labelText).addClass('hint');
   //3. Remove the label element
   label.remove();
   //4.Bind a focus event to the search input that removes the hint text and the "hint" class
   //5.Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
   searchInput.bind('focus', function() {
-  if ($(this).val() == labelText && $(this).hasClass('hint')) {
+  if ($(this).val() == labelText) {
       $(this).val('').removeClass('hint');
     }
 	})
 	.bind('blur', function() {	 
-	  if($(this).val().trim()=="") {
+	  if($(this).val().trim() == "") {
       $(this).val(labelText).addClass('hint');
     }      
   });
