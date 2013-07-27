3f56b53b9d2b3e064d6badb90dc2976543650fe6
divya <divya@vinsol.com>
2013-07-27 12:46:31 +0530

    issues resolved

--- exercise6/js/blog.js
+++ exercise6/js/blog.js
@@ -1,7 +1,7 @@
 //reveal hidden text
 $(document).ready(function() {
   var $blogElement = $('#blog');
-  $blogElement.find('h3').click(function (event) {
+  $blogElement.find('h3').click(function(event) {
     event.preventDefault();
     // finds the paragraphs with the class excerpt and slides it down and slides up the other siblings.
     $(this).siblings('p.excerpt').slideDown(300);
