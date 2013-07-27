b879b2cfb08961c86461ae2a886f0d3bfaa9b4e5
divya <divya@vinsol.com>
2013-07-27 13:26:49 +0530

    issues resolved

--- hiddenexercise3/hidden3.js
+++ hiddenexercise3/hidden3.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  var array=[];
   //by default all radio buttons are disabled
   $(":radio").attr("disabled", true);
   // a click event bind to the rating parameters.
@@ -8,18 +7,11 @@ $(document).ready(function () {
     $(this).addClass('highlight').siblings().removeClass('highlight');
     //if user simly clicks on the ratings wihtout selecting the product nothings happens.
     if($('td.product.highlight').length > 0) {
-      $name=$('td.product.highlight').attr('id');
-      $value = $(this).attr('id');
+      var $name = $('td.product.highlight').attr('id');
+      var $value = $(this).attr('id');
       $(":radio[value='" + $value + "'][name='" +$name+"']").attr("checked", true);
-      //checking if the radiobutton belongs to the same class..i.e. we are changing our ratings for a particular product and if it is we  should make sure that only selected radiobuttons are enabled.
-      var index = array.map(function(el){ return el[1];}).indexOf($name);
-      if(index >=0) {
-        array.splice(index, 1);
-      }
-      array.push([$value,$name]);
-      //enabling only selected radiobuttons.
-      for(i=0;i<array.length;i++){
-        $(":radio[value='" + array[i][0] + "'][name='" +array[i][1] +"']").removeAttr('disabled');
+      for(var i=0;i<$('input[type="radio"]:checked').length;i++) {
+        $('input[type="radio"]:checked').removeAttr('disabled');
       }
     }
     else {  
@@ -29,7 +21,7 @@ $(document).ready(function () {
   //a click function bind to the product
   $('td.product').bind('click', function() {
     $('td.ratingparameters').removeClass('highlight');
-    $(this).closest('#grid').find('.product').removeClass('highlight');
+    $('#grid').find('.product').removeClass('highlight');
     $(this).addClass('highlight');
   });
 });
