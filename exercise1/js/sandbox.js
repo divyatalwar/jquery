13f68e4e8aa605981011773b6261070ecd721673
divya <divya@vinsol.com>
2013-07-29 16:44:24 +0530

    loop removed and corrections made

--- exercise1/js/sandbox.js
+++ exercise1/js/sandbox.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  //solution to 1
+  //solution to 1.
   alert($('div.module').html());
-  //solution to 2
+  //solution to 2.
   alert($('#myList li:nth-child(3)').html());
   alert($('#myList li:eq(2)').html());
   alert($('#myList li:contains("item 3")');
-  //solution to 3 
+  //solution to 3. 
   alert($("label[for='q']").html());
-  //solution to 4
+  //solution to 4.
   alert($(':hidden').length);
-  //solution to 5
+  //solution to 5.
   alert($('img[alt]').length);
-  //solution to 6
+  //solution to 6.
   $('tr:odd').length ;
   });
 });
