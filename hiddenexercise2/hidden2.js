cfba5008ff2488d7580c489db6b648cff711779f
divya <divya@vinsol.com>
2013-07-29 10:25:49 +0530

    review

---
+++ hiddenexercise2/hidden2.js
@@ -0,0 +1,101 @@
+
+jsonArray = {"clothing":[{"name":"clothing product1" , "price":500, "description":"it is a clothing product intended to give specific  functionality"},
+{"name":"clothing product2" , "price":600, "description":"it is a clothing product intended to give specific  functionality"},
+{"name":"clothing product3" , "price":700, "description":"it is a clothing product intended to give specific  functionality"}],
+"electronics":[{"name":"electronics product1" , "price":700, "description":"it is a electronics product intended to give specific  functionality"},
+{"name":"electronics product2" , "price":300, "description":"it is a electronics product intended to give specific  functionality"},
+{"name":"electronics product3" , "price":200, "description":"it is a electronics product intended to give specific  functionality"}],
+"grocery":[{"name":"grocery product1" , "price":400, "description":"it is a grocery product intended to give specific  functionality"},
+{"name":"grocery product2" , "price":100, "description":"it is a grocery product intended to give specific  functionality"},
+{"name":"grocery product3" , "price":300, "description":"it is a grocery product intended to give specific  functionality"}]}
+
+
+$(document).ready(function() {
+  $('#mycart1').hide();
+  var cart = new myCart();
+  $('#container').delegate('#types', 'change', function(){
+    item = $('#types option:selected').val();
+    cart.displayCart(item);
+  });
+  $('#container').delegate('#addToCart', 'click', function(){
+    var id = $(this).parent().attr('id');
+    cart.addToCart(id);
+  });
+  $('#container').delegate('.remove', 'click', function(){
+    var id = $(this).attr('id');
+    cart.remove(id);
+  });
+  $('#container').delegate('#mycart', 'click', function(){
+    $('#mycart1').show();
+    $('#lowercontent').hide();
+    $('#lowerportion').hide();
+  });
+  $('#container').delegate('#products', 'click', function(){
+    $('#mycart1').hide();
+    $('#lowercontent').show();
+    $('#lowerportion').show();
+  });
+});
+function myCart(){
+  var kart=this;
+  this.displayCart = function(item){
+    $('#products').addClass('border');
+    $('div.myDiv').remove();
+    for(var i = 0; i < jsonArray[item].length; i++) {
+        var newDiv = $('#myproduct').clone();
+        $(newDiv).removeClass('hidden').addClass('myDiv').attr({'id':i+"_"+item,'class':i+"_"+item}).find('#myName').text(" name:" + jsonArray[item][i].name).siblings('#mycontent').text(" content: " + jsonArray[item][i].description).siblings('#myPrice').text(" Price: Rs." + jsonArray[item][i].price);           
+        $('#lowercontent').append(newDiv);
+     }
+    }
+  this.addToCart = function(myId){
+    var myRegex=/^[0-9]+$/;
+    var bool= false;
+    id = myId.split("_");
+    item=id[1];
+    product=id[0];
+    var productName = jsonArray[item][product].name;
+    var price = jsonArray[item][product].price;
+    var quantity = $('.'+ myId).find('input').last().val();
+    var subTotal = price*quantity;
+    if(myRegex.test(quantity)){
+      $('#tableid tr').each(function() {
+        if($(this).attr('id')==myId) {
+          $(this).find('td:nth-child(3)').text(quantity);
+          $(this).find('td:nth-child(4)').text(subTotal);
+          bool=true;
+          }
+      });
+      $('#'+ myId).find('input').val("");
+      if(!bool) {
+        $('#tableid').append('<tr id="' +myId +'"</td><td>' + productName + '</td><td>' + price + '</td><td>' + quantity + '</td><td>' + subTotal + '</td><td><input type="submit" value="remove" class="remove" id="remove_' + myId + '"></td></tr>');
+}
+      kart.calculateTotal();
+      $('#mycart').text('My Cart (' + ($('#tableid tr').length-1) + ')');   
+    }
+    else {
+      alert("please specify the valid quantity");
+    }
+  }
+  this.calculateTotal=function() {
+    var total=0;
+    $('#tableid td:nth-child(4)').each(function() {
+      total+=parseInt($(this).text());
+    });
+    $('#totalprice').val(total);
+  }
+  this.remove = function(id){
+    $('#' + id).closest('tr').remove();
+    kart.calculateTotal();
+    $('#totalprice').val(total);
+  }
+}
+
+
+
+
+
+
+
+
+
+
