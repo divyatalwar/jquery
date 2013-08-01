39a9a0564a6c1a9798f5cf9094141479337ef92f
divya <divya@vinsol.com>
2013-08-01 10:17:30 +0530

    first technique adopted

--- productgrid/hidden3.js
+++ productgrid/hidden3.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   var brandId,brandColor;
   $.ajax({
@@ -12,94 +11,36 @@ $(document).ready(function() {
         brand = jsonData[i].brand;
         sold_out = jsonData[i].sold_out;
         color = jsonData[i].color;
-      	$innerDiv = $('<div id="' + i + '"class="inner"/>');
-        $innerDiv.appendTo($('#outerDiv')).addClass('a').attr({ 'data-brand':brand,'data-color':color,'data-sold_out':sold_out});
-        $innerDiv.append('<p><img class="a" src="../productgrid/'  + url +'"/>');
+      	$innerDiv = $('<div id="' + i + '"class="a"/>');
+        $innerDiv.appendTo($('#outerDiv')).attr({ 'data-brand':brand,'data-color':color,'data-sold_out':sold_out});
+        $innerDiv.append('<img  src="../productgrid/'  + url +'"/>');
       }
     }
   });
-  $('#filter').delegate('input[type="checkbox"]','click',function() {
-    $('#outerDiv').show();    
-    $('.inner').addClass("inner1");
+  $('#filter').delegate('input','click',function() {
+    $('.a').addClass('hidden');    
     filter();     
   });
   function filter() {
-    var filterByBrand = [], filterByColor =[],jsonFinal=[];
-    var i,j;
-    a=[];
-    filterByBrand= filterBy("brand");
-    filterByColor=filterBy("color");
-    if(filterByBrand.length==0) {
-      if(filterByColor.length==0)
-      $('.inner').removeClass('hidden');
-      else {
-        for(i=0;i<filterByColor.length;i++){
-          jsonFinal.push($("#"+filterByColor[i]));
-        }
-      }
-      display(jsonFinal);
+    var a = null;
+    a = filterby("brand",$('.a')); 
+    a = filterby("color",a);
+    if($('#available:checked').length){
+      a = filterby("sold_out",a);
+    }
+    a.removeClass('hidden');
+  }
+  function filterby(element,jqueryobject) {
+    var myselector = "";
+    if($('#my' + element + ' :input:checked').length > 0) {
+      $('#my' + element + ' :input:checked').each( function(index) {
+        myselector += (index == 0)? "" :", ";
+        myselector += '[data-' + element + '="' + $(this).val() + '"]';
+    });
+      return jqueryobject.filter(myselector);
     }
     else {
-      if(filterByColor.length==0){
-        for(i=0;i<filterByBrand.length;i++) {
-          jsonFinal.push($("#"+filterByBrand[i]));
-        }
-        display(jsonFinal);
-      }
-      else {
-        for(i=0;i<filterByBrand.length;i++){
-          for(j=0;j<filterByColor.length;j++){
-            if (filterByBrand[i] == filterByColor[j]) {
-              jsonFinal.push($("#"+filterByBrand[i]));
-            }
-          }
-        }
-        display(jsonFinal);
-      } 
+      return jqueryobject;
     }
   }
-  function display(myImages) {
-    for(var i = 0; i < myImages.length; i++){
-	    if($('#available:checked').length>0) {
-        $('#available').parent().siblings().find('input').attr('checked',false);
-        if(myImages[i].attr('data-sold_out') ==0) {
-          myImages[i].addClass('hidden');
-        }
-      }
-      else
-        myImages[i].addClass('hidden');
-    }
-  }
-  function filterBy(element) {
-    selected_Id=[];
-    a=[];
-    $('#my'+element).find('input[type="checkbox"]').each(function() {
-      $(this).filter(":checked").each(function() {
-  			a=findImages($(this).attr('name'),$(this).attr('value'));
-      });
-    });
-    return a;
-  }
-  function findImages(name ,value) {
-    $('.inner').each( function () {
-      $(this).removeClass("hidden");
-      if ($(this).attr('data-'+name) == value ) {
-        selected_Id.push($(this).attr('id'));
-      }
-   });
-   return selected_Id;
-  }
-  $('#sold_out input').click(function() 
-    { 
-      if(this.checked === true)
-      {
-        $(this).parent().siblings().find('input').attr('checked',false);
-	
-      }
-      else
-      {
-        $(this).parent().siblings().find('input').attr('checked',false);
-      }
-      
-    });
 });
