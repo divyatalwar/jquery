8e5fd69007438c7439d4f4915f8afe4fcccbd3eb
divya <divya@vinsol.com>
2013-08-02 11:41:38 +0530

    appending soln chnaged

--- exercise3/js/sandbox.js
+++ exercise3/js/sandbox.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
   //solution to 1.
-  var listArray = [];
-  var divArray = [];
   var list = $('#myList');
   for(var i = 0 ; i < 5 ; i++) {
-    listArray.push('<li>item' + (i+1) +'</li>');
+    ($('<li>item' + (i+1)+'</li>')).appendTo(list);
   }
-  $('#myList').append(listArray.join(''));
   //solution to 2.
   $('#myList li:odd').remove();
   //solution to 3
-  divArray.push('<h2> heading added </h2><p> paragraph added </p>');
-  $('div.module:last').append(divArray.join(''));
+  $('div.module:last').append($('<h2> heading added </h2><p> paragraph added </p>'));
   //solution to 4
   $('#specials select[name="day"]').append("<option value='wednesday'>wednesday</option>");
   //solution to 5
-  $('<div class="module"/>').insertAfter('div.module:last');
-  $('*').find('img').first().clone().appendTo('div.module:last');
+  var newDiv= $('<div class="module">');
+  newDiv.append($('img:first').clone()).insertAfter('div.module:last');
 });
