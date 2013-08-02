4b10141e6e779e9dacbf7cb08099b99e8f8dd448
divya <divya@vinsol.com>
2013-08-02 14:33:40 +0530

    global variables avoided.properly indented

--- productgrid/hidden3.js
+++ productgrid/hidden3.js
@@ -1,42 +1,43 @@
 $(document).ready(function() {
+  var jsonData = null;
   $.ajax({
     url: "product.json",
     type: "GET",
     dataType: "json",
     success: function(data){
       jsonData = data;   
-      for(i = 0;i < jsonData.length;i = i + 1) {
-        url = 'images/' + jsonData[i].url;
-        brand = jsonData[i].brand;
-        sold_out = jsonData[i].sold_out;
-        color = jsonData[i].color;
-      	$innerDiv = $('<div id="' + i + '"class="a"/>');
-        $innerDiv.appendTo($('#outerDiv')).attr({ 'data-brand':brand,'data-color':color,'data-sold_out':sold_out});
+      var outerDiv=$('#outerDiv');
+      for(var i = 0; i < jsonData.length; i = i + 1) {
+        var url = 'images/' + jsonData[i].url;
+        var brand = jsonData[i].brand;
+        var sold_out = jsonData[i].sold_out;
+        var color = jsonData[i].color;
+      	var $innerDiv = $('<div id="' + i + '"class="innerdiv">');
+        $innerDiv.appendTo(outerDiv).attr({ 'data-brand':brand,'data-color':color,'data-sold_out':sold_out});
         $innerDiv.append('<img  src="../productgrid/'  + url +'"/>');
       }
     }
   });
   $('#filter').delegate('input','click',function() {
-    $('.a').addClass('hidden');    
+    $('.innerdiv').addClass('hidden');    
     filter();     
   });
   function filter() {
-    var a = null;
-    a = filterby("brand",$('.a')); 
-    a = filterby("color",a);
-    if($('#available:checked').length){
-      a = filterby("sold_out",a);
+    var filtered_set = $('.innerdiv');
+    filtered_set = filterby("brand", filtered_set); 
+    filtered_set = filterby("color", filtered_set);
+    if ($('#available:checked').length){
+      filtered_set = filterby("sold_out", filtered_set);
     }
-    a.removeClass('hidden');
+    filtered_set.removeClass('hidden');
   }
-  function filterby(element,jqueryobject) {
-    var myselector = "";
-    if($('#my' + element + ' :input:checked').length > 0) {
-      $('#my' + element + ' :input:checked').each( function(index) {
-        myselector += (index == 0)? "" :", ";
-        myselector += '[data-' + element + '="' + $(this).val() + '"]';
-    });
-      return jqueryobject.filter(myselector);
+  function filterby(element, jqueryobject) {
+    var myselector = [];
+    if ($('#my' + element + ' :input:checked').length > 0) {
+      $('#my' + element + ' :input:checked').each(function(index) {
+        myselector.push('[data-' + element + '="' + $(this).val() + '"]');
+      });
+      return jqueryobject.filter(myselector.join(','));
     }
     else {
       return jqueryobject;
