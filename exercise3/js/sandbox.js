94602d38b2f3ae6e53321a732f7dee54a5f548d8
divya <divya@vinsol.com>
2013-08-07 01:02:27 +0530

    plain text avoided and img syntax updation

--- exercise3/js/sandbox.js
+++ exercise3/js/sandbox.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
   //solution to 1.
   var list = $('#myList');
   for(var i = 0 ; i < 5 ; i++) {
-    ($('<li>').text('item' + (i+1))).appendTo(list);
+    ($('<li />').text('item' + (i+1))).appendTo(list);
   }
   //solution to 2.
   $('#myList li:odd').remove();
   //solution to 3
-  $('div.module:last').append($('<h2> heading added </h2><p> paragraph added </p>'));
+  $('div.module:last').append($('<h2 />').text(' heading added '))
+                      .append($('<p />').text('a paragraph added'));
   //solution to 4
   $('#specials select[name="day"]').append("<option value='wednesday'>wednesday</option>");
   //solution to 5
   var newDiv= $('<div class="module">');
-  newDiv.append($('img:first').clone()).insertAfter('div.module:last');
+  newDiv.append($('img').first().clone()).insertAfter('div.module:last');
 });
