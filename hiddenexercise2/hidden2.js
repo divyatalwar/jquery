8233aae245a81d9187582045db084beccda0aeb0
divya <divya@vinsol.com>
2013-08-09 11:08:07 +0530

    global variables avoided

--- hiddenexercise2/hidden2.js
+++ hiddenexercise2/hidden2.js
@@ -1,6 +1,5 @@
-
-$(document).ready(function() {
-  jsonArray = {"clothing":[{"name":"clothing product1" , "price":500, "description":"it is a clothing product intended to give specific  functionality"},
+$(function() {
+  var jsonArray = {"clothing":[{"name":"clothing product1" , "price":500, "description":"it is a clothing product intended to give specific  functionality"},
   {"name":"clothing product2" , "price":600, "description":"it is a clothing product intended to give specific  functionality"},
   {"name":"clothing product3" , "price":700, "description":"it is a clothing product intended to give specific  functionality"}],
   "electronics":[{"name":"electronics product1" , "price":700, "description":"it is a electronics product intended to give specific  functionality"},
@@ -10,7 +9,7 @@ $(document).ready(function() {
   {"name":"grocery product2" , "price":100, "description":"it is a grocery product intended to give specific  functionality"},
   {"name":"grocery product3" , "price":300, "description":"it is a grocery product intended to give specific  functionality"}]}
 
-  var mycart = new cart();
+  var mycart = new cart(jsonArray);
   mycart.displayCart("all");
   $('#container').delegate('#mycart , #products', 'click', function(e) {
     $(this).siblings().addClass('hidden');
@@ -20,13 +19,14 @@ $(document).ready(function() {
   });
   $('#container').delegate('input[value="search"]', 'click', function() {
     var item=$('#search').val();
-    mycart.display(item);   
+    mycart.selectiveDisplay(item);   
   });  
   $('#container').delegate('input[value="clear"]', 'click', function() {
-    $('#search').val("");     
+    $('#search').val("");
+    $('.myDiv').removeClass('hidden');    
   });
   $('#container').delegate('#types', 'change', function(){
-    item=$(this).val();
+    var item=$(this).val();
     mycart.display(item);
   });
   $('#container').delegate('#add', 'click', function(){
@@ -40,7 +40,7 @@ $(document).ready(function() {
   $('#container').delegate('#checkout','click',function() {
     $('#container').html('<div style="text-align:center">Ooopss!! u exited the shopping cart.thanku for visiting<br><input type="submit" value="home" id="home"></div>');
   });
-  $('body').delegate("#home","click",function (event) {
+  $('body').delegate("#home","click",function(event) {
     location.reload();
   });
   $('#tableid').delegate('.changeValue', 'focusout', function(){
@@ -52,7 +52,8 @@ $(document).ready(function() {
     mycart.calculateTotal(id);
   }); 
 });
-function cart() {
+var cart=function(jsonCart) {
+  var jsonArray=jsonCart;
   var kart=this;
   this.displayCart=function(item) {
     $('.myDiv').remove();
@@ -61,12 +62,13 @@ function cart() {
       if (jsonArray.hasOwnProperty(key)){         
         for(var i = 0; i < jsonArray[key].length; i++){
           var newDiv = $('#myproduct').clone();
-          $(newDiv).removeClass('hidden').attr({'id':i+"_"+key,'class':key+' '+'myDiv mycss'}).find('#myName').text(" name:" + jsonArray[key][i].name).siblings('#mycontent').text(" content: " + jsonArray[key][i].description).siblings('#myPrice').text(" Price: Rs." + jsonArray[key][i].price);           
+          $(newDiv).removeClass('hidden').attr({'id':i+"_"+key,'class':key+' '+'myDiv mycss'}).children('.myName').text(" name:" + jsonArray[key][i].name).siblings('.mycontent').text(" content: " + jsonArray[key][i].description).siblings('.myPrice').text(" Price: Rs." + jsonArray[key][i].price);           
           $('#lowercontent').append(newDiv);
         }
       }
     }
   }
+
   this.display=function(item) {
     if(item != 'all') {
       $('.myDiv').addClass('hidden');
@@ -74,18 +76,17 @@ function cart() {
     }
     else {
       $('.myDiv').removeClass('hidden');
-    }
-      
+    } 
+  }
+  this.selectiveDisplay=function(item) {
+    $('.myDiv').addClass('hidden');
+    $(".myDiv:contains("+item+")").removeClass('hidden');
   }
   this.addToCart=function(myId){
     var myRegex=/^[0-9]+$/;
-    var bool= false;
-    var id = myId.split("_");
-    var item=id[1];
-    var product=id[0];
-    var productName = jsonArray[item][product].name;
-    var price = jsonArray[item][product].price;
-    var quantity = $('#'+ myId).find('input').val();
+    var productName=$('#'+myId).children('.myName').text();
+    var price=$('#'+myId).children('.myPrice').text().split("Rs.")[1];    
+    var quantity = $('#'+ myId).children('input').val();
     var subTotal = price*quantity;
     if(myRegex.test(quantity)){
       $('#tableid').append('<tr id="row' +myId +'"><td class="product">' + productName + '</td><td class="price">' + price + '</td><td class><input type="text" size="1" class="changeValue" value = "' + quantity + '"></td><td class="subTotal">' + subTotal + '</td><td><input type="submit" value="remove" class="remove" id="remove-' + myId + '"></td></tr>');
@@ -99,7 +100,7 @@ function cart() {
     }
   }
   this.calculateTotal=function() {
-    total=0;
+    var total=0;
     $('td.subTotal').each(function() {
     total+=parseInt($(this).text());
   });
@@ -109,7 +110,12 @@ function cart() {
     $("#"+id).remove();
     kart.calculateTotal();
     $('#totalprice').val(total);
-    $('#mycart').text('My Cart ('+($('#tableid tr').length-1) +')');
+    if ($('#tableid tr').length-1) {
+      $('#mycart').text('My Cart ('+($('#tableid tr').length-1) +')');
+    }
+    else {
+      $('#mycart').text('My Cart ');
+    }
   }
 }
 
